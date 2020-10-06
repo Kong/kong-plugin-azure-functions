827be6fe29f8ba8ac39bd3cf121a3052663f07c6
Javier Guerra <javier.guerra@konghq.com>
2020-10-05 22:33:48 -0500

    tests - add tests for buffered request and/or response

--- spec/01-access_spec.lua
+++ spec/01-access_spec.lua
@@ -6,160 +6,164 @@ local server_tokens = meta._SERVER_TOKENS
 
 
 for _, strategy in helpers.each_strategy() do
-  describe("Plugin: Azure Functions (access) [#" .. strategy .. "]", function()
-    local proxy_client
+  for buffering_request, buffering_response, buffering_label in helpers.each_buffering() do
+    describe("Plugin: Azure Functions (access) [#" .. strategy .. "](".. buffering_label ..")", function()
+      local proxy_client
 
-    setup(function()
-      local bp = helpers.get_db_utils(strategy)
+      setup(function()
+        local bp = helpers.get_db_utils(strategy)
 
-      local route2 = bp.routes:insert {
-        hosts      = { "azure2.com" },
-        protocols  = { "http", "https" },
-        service    = bp.services:insert({
-          protocol = "http",
-          host     = "to.be.overridden",
-          port     = 80,
+        local route2 = bp.routes:insert {
+          hosts      = { "azure2.com" },
+          protocols  = { "http", "https" },
+          service    = bp.services:insert({
+            protocol = "http",
+            host     = "to.be.overridden",
+            port     = 80,
+          }),
+          request_buffering = buffering_request,
+          response_buffering = buffering_response,
+        }
+
+        -- this plugin definition results in an upstream url to
+        -- http://httpbin.org/anything
+        -- which will echo the request for inspection
+        bp.plugins:insert {
+          name     = "azure-functions",
+          route    = { id = route2.id },
+          config   = {
+            https           = true,
+            appname         = "httpbin",
+            hostdomain      = "org",
+            routeprefix     = "anything",
+            functionname    = "test-func-name",
+            apikey          = "anything_but_an_API_key",
+            clientid        = "and_no_clientid",
+          },
+        }
+
+        assert(helpers.start_kong{
+          database = strategy,
+          plugins  = "azure-functions",
         })
-      }
 
-      -- this plugin definition results in an upstream url to
-      -- http://httpbin.org/anything
-      -- which will echo the request for inspection
-      bp.plugins:insert {
-        name     = "azure-functions",
-        route    = { id = route2.id },
-        config   = {
-          https           = true,
-          appname         = "httpbin",
-          hostdomain      = "org",
-          routeprefix     = "anything",
-          functionname    = "test-func-name",
-          apikey          = "anything_but_an_API_key",
-          clientid        = "and_no_clientid",
-        },
-      }
+      end) -- setup
 
-      assert(helpers.start_kong{
-        database = strategy,
-        plugins  = "azure-functions",
-      })
+      before_each(function()
+        proxy_client = helpers.proxy_client()
+      end)
 
-    end) -- setup
+      after_each(function ()
+        proxy_client:close()
+      end)
 
-    before_each(function()
-      proxy_client = helpers.proxy_client()
-    end)
-
-    after_each(function ()
-      proxy_client:close()
-    end)
-
-    teardown(function()
-      helpers.stop_kong()
-    end)
+      teardown(function()
+        helpers.stop_kong()
+      end)
 
 
-    it("passes request query parameters", function()
-      local res = assert(proxy_client:send {
-        method  = "GET",
-        path    = "/",
-        query   = { hello = "world" },
-        headers = {
-          ["Host"] = "azure2.com"
-        }
-      })
+      it("passes request query parameters", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/",
+          query   = { hello = "world" },
+          headers = {
+            ["Host"] = "azure2.com"
+          }
+        })
 
-      assert.response(res).has.status(200)
-      local json = assert.response(res).has.jsonbody()
-      assert.same({ hello ="world" }, json.args)
-    end)
+        assert.response(res).has.status(200)
+        local json = assert.response(res).has.jsonbody()
+        assert.same({ hello ="world" }, json.args)
+      end)
 
-    it("passes request body", function()
-      local body = "I'll be back"
-      local res = assert(proxy_client:send {
-        method  = "GET",
-        path    = "/",
-        body    = body,
-        query   = { hello = "world" },
-        headers = {
-          ["Host"] = "azure2.com"
-        }
-      })
+      it("passes request body", function()
+        local body = "I'll be back"
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/",
+          body    = body,
+          query   = { hello = "world" },
+          headers = {
+            ["Host"] = "azure2.com"
+          }
+        })
 
-      assert.response(res).has.status(200)
-      local json = assert.response(res).has.jsonbody()
-      assert.same(body, json.data)
-    end)
+        assert.response(res).has.status(200)
+        local json = assert.response(res).has.jsonbody()
+        assert.same(body, json.data)
+      end)
 
-    it("#only passes the path parameters", function()
-      local res = assert(proxy_client:send {
-        method  = "GET",
-        path    = "/and/then/some",
-        headers = {
-          ["Host"] = "azure2.com"
-        }
-      })
+      it("#only passes the path parameters", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/and/then/some",
+          headers = {
+            ["Host"] = "azure2.com"
+          }
+        })
 
-      assert.response(res).has.status(200)
-      local json = assert.response(res).has.jsonbody()
-      assert.matches("httpbin.org/anything/test%-func%-name/and/then/some", json.url)
-    end)
+        assert.response(res).has.status(200)
+        local json = assert.response(res).has.jsonbody()
+        assert.matches("httpbin.org/anything/test%-func%-name/and/then/some", json.url)
+      end)
 
-    it("passes the method", function()
-      local res = assert(proxy_client:send {
-        method  = "POST",
-        path    = "/",
-        headers = {
-          ["Host"] = "azure2.com"
-        }
-      })
+      it("passes the method", function()
+        local res = assert(proxy_client:send {
+          method  = "POST",
+          path    = "/",
+          headers = {
+            ["Host"] = "azure2.com"
+          }
+        })
 
-      assert.response(res).has.status(200)
-      local json = assert.response(res).has.jsonbody()
-      assert.same("POST", json.method)
-    end)
+        assert.response(res).has.status(200)
+        local json = assert.response(res).has.jsonbody()
+        assert.same("POST", json.method)
+      end)
 
-    it("injects the apikey and clientid", function()
-      local res = assert(proxy_client:send {
-        method  = "POST",
-        path    = "/",
-        headers = {
-          ["Host"] = "azure2.com"
-        }
-      })
+      it("injects the apikey and clientid", function()
+        local res = assert(proxy_client:send {
+          method  = "POST",
+          path    = "/",
+          headers = {
+            ["Host"] = "azure2.com"
+          }
+        })
 
-      assert.response(res).has.status(200)
-      local json = assert.response(res).has.jsonbody()
-      --assert.same({}, json.headers)
-      assert.same("anything_but_an_API_key", json.headers["X-Functions-Key"])
-      assert.same("and_no_clientid", json.headers["X-Functions-Clientid"])
-    end)
+        assert.response(res).has.status(200)
+        local json = assert.response(res).has.jsonbody()
+        --assert.same({}, json.headers)
+        assert.same("anything_but_an_API_key", json.headers["X-Functions-Key"])
+        assert.same("and_no_clientid", json.headers["X-Functions-Clientid"])
+      end)
 
-    it("returns server tokens with Via header", function()
-      local res = assert(proxy_client:send {
-        method  = "GET",
-        path    = "/",
-        query   = { hello = "world" },
-        headers = {
-          ["Host"] = "azure2.com"
-        }
-      })
+      it("returns server tokens with Via header", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/",
+          query   = { hello = "world" },
+          headers = {
+            ["Host"] = "azure2.com"
+          }
+        })
 
-      assert.equal(server_tokens, res.headers["Via"])
-    end)
+        assert.equal(server_tokens, res.headers["Via"])
+      end)
 
-    it("returns Content-Length header", function()
-      local res = assert(proxy_client:send {
-        method  = "GET",
-        path    = "/",
-        query   = { hello = "world" },
-        headers = {
-          ["Host"] = "azure2.com"
-        }
-      })
+      it("returns Content-Length header", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/",
+          query   = { hello = "world" },
+          headers = {
+            ["Host"] = "azure2.com"
+          }
+        })
 
-      assert(tonumber(res.headers["Content-Length"]) > 100)
-    end)
+        assert(tonumber(res.headers["Content-Length"]) > 100)
+      end)
 
-  end) -- describe
+    end) -- describe
+  end
 end
